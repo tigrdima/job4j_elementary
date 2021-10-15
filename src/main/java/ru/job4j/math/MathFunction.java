7a01dea5345e512edcd642fcca2cbe66ac8452cc
Dima Sh <dyus79@yandex.ru>
2021-10-15 19:22:35 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -12,7 +12,8 @@ public class MathFunction {
     public static double minus(double first, double second) {
         return first - second;
     }
-     public static double division(double first, double second) {
+
+    public static double division(double first, double second) {
             return first / second;
         }
     }
