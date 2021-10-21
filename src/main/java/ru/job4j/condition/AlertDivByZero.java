02c6f8226c84b8d7960ffd913ff9fb019bc9dce5
Dima Sh <dyus79@yandex.ru>
2021-10-21 12:53:24 +0300

    4.1. Операторы ветвлений [#3611 #217656]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 public class AlertDivByZero {
     public static void main(String[] args) {
         possibleDiv(-2);
+        possibleDiv(0);
     }
 
     public static void possibleDiv(int number) {
