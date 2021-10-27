d00bb560d96bb8b0d70e01c054b8455dc59e284a
Dima Sh <dyus79@yandex.ru>
2021-10-27 19:33:28 +0300

    5.0.1. Сумма чисел. [#226416 #220892]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,16 +9,6 @@ public class Counter {
         return sum;
     }
 
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            if (i % 2 == 0) {
-                sum = sum + i;
-            }
-        }
-        return sum;
-    }
-
     public static void main(String[]args) {
             System.out.println(Counter.sum(0, 0));
             System.out.println(Counter.sum(1, 3));
