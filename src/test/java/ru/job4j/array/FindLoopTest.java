b4ecb8d5bdbe9e1561bf66a6d4af43967a1cdd50
Dima Sh <dyus79@yandex.ru>
2021-11-01 20:14:49 +0300

    6.1. Тест - Классический поиск перебором. [#33489 #221851]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -29,7 +29,7 @@ public class FindLoopTest {
         int[] data = {5, 4, 3, 2};
         int el = 1;
         int result = FindLoop.indexOf(data, el);
-        int expected = - 1;
+        int expected = -1;
         Assert.assertEquals(expected, result);
     }
 }
