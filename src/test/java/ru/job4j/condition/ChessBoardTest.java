52b6e211cc8bda23b71f986fef92fc3bb9979e15
Dima Sh <dyus79@yandex.ru>
2021-11-08 21:18:01 +0300

    4.3.6. Тест - Math.abs [#373616]

---
+++ src/test/java/ru/job4j/condition/ChessBoardTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class ChessBoardTest {
+
+    @Test
+    public void wayIs5() {
+        int x1 = 6;
+        int y1 = 7;
+        int x2 = 1;
+        int y2 = 2;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 5;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void wayIs7() {
+        int x1 = 7;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 7;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 7;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void wayIs0() {
+        int x1 = 2;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 1;
+        int result = ChessBoard.way(x1, y1, x2, y2);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+}
